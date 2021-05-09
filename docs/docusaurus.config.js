1ce4410dbc191e028339a2e6eb78351f86b776d4
= <=>
2021-05-09 03:51:32 +0100

    e

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: 'Transform GUI',
   tagline: 'A ROBLOX module to create draggable and resizeable guis easily',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://itsajhere.github.io/TransformGUI/',
+  baseUrl: '/TransformGUI/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'itsajhere', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'TransformGui', // Usually your repo name.
   themeConfig: {
     colorMode: {
       defaultMode: "dark"
