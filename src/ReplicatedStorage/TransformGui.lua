6b84cec2e87684fd156cb21983b73537bf9bc39e
kent <38088995+kent911t@users.noreply.github.com>
2021-05-31 21:22:37 +0100

    Corner dragging, Resize icon
    corner dragging
    dragging outside frame area
    additional parameter in constructor - takes "Resize", "Drag", "ResizeDrag"
    changed constructor parameter ignoreGuiInsetEnabled to isTopBarEnabled; default to true
    if guiobject is a frame automatically disables built in dragging
    
    problems:
    dragging and resizing cause race condition; temporary solution to just return on dragging when resizing

--- src/ReplicatedStorage/TransformGui.lua
+++ src/ReplicatedStorage/TransformGui.lua
@@ -1,14 +1,22 @@
 local uis = game:GetService("UserInputService")
+local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+local textures = {
+	["Y"] = "rbxasset://textures/StudioUIEditor/icon_resize4.png",
+	["X"] = "rbxasset://textures/StudioUIEditor/icon_resize2.png",
+	["CornerTLBR"] = "rbxasset://textures/StudioUIEditor/icon_resize3.png",
+	["CornerBLTR"] = "rbxasset://textures/StudioUIEditor/icon_resize1.png",
+}
+
 local TransformGui = {}
 TransformGui.__index = TransformGui
 
-function TransformGui.new(guiObject: GuiObject, ignoreGuiInsetEnabled: boolean)
+function TransformGui.new(guiObject: GuiObject, frameProperty: string, isTopBarEnabled: boolean)
 	local self = setmetatable({}, TransformGui)
 	self.guiObject = guiObject
-	self.ignoreGuiInsetEnabled = ignoreGuiInsetEnabled
+	self.isTopBarEnabled = isTopBarEnabled or true
 	self.resizing = false
 	self.dragging = false
-
+	
 	-- Events
 	self._onResizeBeginEvent = Instance.new("BindableEvent")
 	self._onResizeChangedEvent = Instance.new("BindableEvent")
@@ -22,7 +30,26 @@ function TransformGui.new(guiObject: GuiObject, ignoreGuiInsetEnabled: boolean)
 	self.onDragBegin = self._onDragBeginEvent.Event
 	self.onDragChanged = self._onDragChangedEvent.Event
 	self.onDragEnded = self._onDragEndedEvent.Event
+	
+	if guiObject:IsA("Frame") then
+		guiObject.Active = false
+		guiObject.Selectable = false
+	end
+	
+	self:ApplyFrameProperties(frameProperty)
+	
 	return self
+end
+
+function TransformGui:ApplyFrameProperties(property)
+	if property == "Resize" then
+		self:makeResizeable()
+	elseif property == "Drag" then
+		self:makeDraggable()
+	elseif property == "ResizeDrag" then
+		self:makeResizeable()
+		self:makeDraggable()
+	end
 end
 
 function TransformGui:makeDraggable()
@@ -39,7 +66,7 @@ function TransformGui:makeDraggable()
 		end
 	end)
 	uis.InputChanged:Connect(function(input, r)
-		if r then return end
+		if r or self.resizing then return end
 		if moving and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
 			local mLocation = uis:GetMouseLocation()
 			self.guiObject.Position = UDim2.new(0, mLocation.X, 0, mLocation.Y) + delta
@@ -56,41 +83,42 @@ function TransformGui:makeDraggable()
 	end)
 end
 
+function inRange(n1, n2, range)
+	return n1 > n2 - range/2 and n1 < n2 + range/2
+end
+
 function TransformGui:makeResizeable()
 	local moving = false
 	local oldMousePos = Vector2.new(0, 0)
+	local isValid = {{false, false}, {false, false}}
+	-- L = Left, R = Right, T = Top, B = Bottom
 	local xL = false
 	local xR = false
 	local yT = false
 	local yB = false
-	self.guiObject.InputBegan:Connect(function(input)
+	local prevMouseIcon = Mouse.Icon
+	uis.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
 			local mLocation = uis:GetMouseLocation()
-			if not self.ignoreGuiInsetEnabled then 
+			if self.isTopBarEnabled then 
 				mLocation -= Vector2.new(0, 36) 
 			end
-			if mLocation.X < self.guiObject.AbsolutePosition.X + self.guiObject.AbsoluteSize.X + 5 and mLocation.X > self.guiObject.AbsolutePosition.X + self.guiObject.AbsoluteSize.X - 5 then
-				moving = true
-				self.resizing = true
-				oldMousePos = mLocation
-				xR = true
-			elseif mLocation.X < self.guiObject.AbsolutePosition.X + 5 and mLocation.X > self.guiObject.AbsolutePosition.X - 5 then
-				moving = true
-				self.resizing = true
-				oldMousePos = mLocation
-				xL = true
-			elseif mLocation.Y < self.guiObject.AbsolutePosition.Y + self.guiObject.AbsoluteSize.Y + 5 and mLocation.Y > self.guiObject.AbsolutePosition.Y + self.guiObject.AbsoluteSize.Y - 5 then
-				moving = true
-				self.resizing = true
-				oldMousePos = mLocation
-				yB = true
-			elseif mLocation.Y < self.guiObject.AbsolutePosition.Y + 5 and mLocation.Y > self.guiObject.AbsolutePosition.Y - 5 then
-				moving = true
-				self.resizing = true
-				oldMousePos = mLocation
-				yT = true
-			end
+			local absPos = self.guiObject.AbsolutePosition -- Top Left
+			local absSize = self.guiObject.AbsoluteSize
+			local endPos = absPos + absSize -- Bottom Right
+			xR = inRange(mLocation.X, absPos.X + absSize.X, 10)
+			xL = not xR and inRange(mLocation.X, absPos.X, 10)
+			yB = inRange(mLocation.Y, absPos.Y + absSize.Y, 10)
+			yT = inRange(mLocation.Y, absPos.Y, 10)
+			
 			if yB or yT or xR or xL then
+				Mouse.Icon = (xR or xL) and not (yB or yT) and textures.X or
+					(yB or yT) and not (xR or xL) and textures.Y or
+					((yT and xL) or (yB and xR)) and textures.CornerTLBR or
+					((yB and xL) or (yT and xR)) and textures.CornerBLTR
+				self.resizing = true
+				moving = true
+				oldMousePos = mLocation
 				self._onResizeBeginEvent:Fire()
 			end
 		end
@@ -99,32 +127,32 @@ function TransformGui:makeResizeable()
 		if r then return end
 		if moving and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
 			local mLocation = uis:GetMouseLocation()
-			if not self.ignoreGuiInsetEnabled then 
+			if self.isTopBarEnabled then 
 				mLocation -= Vector2.new(0, 36) 
 			end
 			if xR then
 				local delta = mLocation - oldMousePos
 				self.guiObject.Size += UDim2.new(0, delta.X, 0, 0)
-				oldMousePos = mLocation
 			elseif xL then
 				local delta = mLocation - oldMousePos
 				self.guiObject.Size -= UDim2.new(0, delta.X, 0, 0)
 				self.guiObject.Position += UDim2.new(0, delta.X, 0, 0)
-				oldMousePos = mLocation
-			elseif yT then
+			end
+			
+			if yT then
 				local delta = mLocation - oldMousePos
 				self.guiObject.Size -= UDim2.new(0, 0, 0, delta.Y)
 				self.guiObject.Position += UDim2.new(0, 0, 0, delta.Y)
-				oldMousePos = mLocation
 			elseif yB then
 				local delta = mLocation - oldMousePos
 				self.guiObject.Size += UDim2.new(0, 0, 0, delta.Y)
-				oldMousePos = mLocation
 			end
+			
+			oldMousePos = mLocation
 			self._onResizeChangedEvent:Fire()
 		end
 	end)
-	self.guiObject.InputEnded:Connect(function(input)
+	uis.InputEnded:Connect(function(input)
 		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and self.resizing then
 			moving = false
 			xL = false
@@ -132,6 +160,7 @@ function TransformGui:makeResizeable()
 			yT = false
 			yB = false
 			self.resizing = false
+			Mouse.Icon = prevMouseIcon
 			self._onResizeEndedEvent:Fire()
 		end
 	end)
